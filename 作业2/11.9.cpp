96a1e18c355b3a39212b83855ee85ecd165630ac
yzr-mint <helloworldyzr@gmail.com>
2023-11-09 20:56:23 +0800

    内插法

--- 作业2/11.9.cpp
+++ 作业2/11.9.cpp
@@ -67,7 +67,36 @@ Datatype interpolate_a(const unsigned int index) {
 	return a[index];
 }
 
+template<typename Datatype>
+void iterate(vector<Datatype>* us, vector<Datatype>* ts, Datatype h, Datatype func(Datatype, Datatype)) {
+	Datatype newu = *(us->end() - 2);
+	vector<Datatype>* fs = get_f(us, ts, func);
+	for (int i = 0; i < us->size(); i++) {
+		newu += interpolate_a<Datatype>(i) * fs->back() * h;
+		fs = delta(fs);
+	}
+	delete fs;
+	*(us->end() - 1) = newu;
+	return;
+}
 
+template<typename Datatype>
+vector<Datatype>* solve_in(vector<Datatype>* u0, vector<Datatype>* t0, Datatype h, Datatype func(Datatype, Datatype), Datatype ub) {
+	vector<Datatype>* result = new vector<Datatype>({ u0->back() });
+	vector<Datatype>* us = new vector<Datatype>(*u0);
+	vector<Datatype>* ts = new vector<Datatype>(*t0);
+	for (Datatype now = ts->back(); now < ub; now += h) {
+		us->push_back(step(us, ts, h, func));
+		result->push_back(us->back());
+		ts->push_back(ts->back() + h);
+		for (int i = 0; i < 10; i++) {
+			iterate(us, ts, h, func);
+		}
+		us->erase(us->begin());
+		ts->erase(ts->begin());
+	}
+	return result;
+}
 
 
 
@@ -76,24 +105,55 @@ Datatype func(Datatype u, Datatype t) {
 	return -5 * u;
 }
 
+// ≤‚ ‘
+#include <fstream>
+
+typedef double datatype;
+
+template<typename Datatype>
+vector<Datatype>* linspace(Datatype lb, Datatype rb, Datatype step) {
+	vector<Datatype>* a = new vector<Datatype>;
+	a->push_back(lb);
+	for (; lb < rb; lb += step) {
+		a->push_back(lb + step);
+	}
+	return a;
+}
+
 // 0.1
-// vector<double>* u0 = new vector<double>({ 4.4816890703 ,2.7182818285 ,1.6487212707 ,1 });
-// vector<double>* t0 = new vector<double>({ -0.3,-0.2,-0.1,0 });
+// vector<datatype>* u0 = new vector<datatype>({ 4.4816890703 ,2.7182818285 ,1.6487212707 ,1 });
+// vector<datatype>* t0 = new vector<datatype>({ -0.3,-0.2,-0.1,0 });
 // 0.05
-// vector<double>* u0 = new vector<double>({ 2.1170000166 ,1.6487212707 ,1.2840254167, 1 });
-// vector<double>* t0 = new vector<double>({ -0.15,-0.1,-0.05,0 });
+// vector<datatype>* u0 = new vector<datatype>({ 2.1170000166 ,1.6487212707 ,1.2840254167, 1 });
+// vector<datatype>* t0 = new vector<datatype>({ -0.15,-0.1,-0.05,0 });
 
 int main() {
-	double now = 0;
-	double h = 0.1;
-	vector<double>* u0 = new vector<double>({ 2.1170000166 ,1.6487212707 ,1.2840254167, 1 });
-	vector<double>* t0 = new vector<double>({ -0.15,-0.1,-0.05,0 });
 
-	auto result = solve<double>(u0, t0, h, func, 1);
-	for (auto i = result->begin(); i != result->end(); i++) {
-		cout << *i << ",";
+	ofstream output;
+	output.open("output.csv", ios_base::out);
+
+	datatype now = 0;
+	vector<datatype>* u0 = new vector<datatype>({ 2.1170000166 ,1.6487212707 ,1.2840254167, 1 });
+	vector<datatype>* t0 = new vector<datatype>({ -0.15,-0.1,-0.05,0 });
+	datatype h = t0->at(1) - t0->at(0);
+
+	vector<datatype>* ts = linspace<datatype>(0, 1, h);
+	for (auto i = ts->begin(); i != ts->end(); i++) {
+		output << *i << ",";
 	}
-	cout << endl;
+	output << endl;
+
+	auto result1 = solve<datatype>(u0, t0, h, func, 1);
+	for (auto i = result1->begin(); i != result1->end(); i++) {
+		output << *i << ",";
+	}
+	output << endl;
+
+	auto result2 = solve_in<datatype>(u0, t0, h, func, 1);
+	for (auto i = result2->begin(); i != result2->end(); i++) {
+		output << *i << ",";
+	}
+	output << endl;
 
 	return 0;
 }
